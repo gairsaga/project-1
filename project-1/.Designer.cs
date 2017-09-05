a0b4644f108df124f109641cb1868ee50ed77921
sagar gaire <gairesagar90@gmail.com>
2017-09-05 17:59:25 -0600

    I finished my assingment

---
+++ project-1/.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace project_1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.btnGoodBye = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(155, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.Text = "World";
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(40, 48);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(75, 23);
+            this.btnSayHello.TabIndex = 1;
+            this.btnSayHello.Text = "Say Hello";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // btnGoodBye
+            // 
+            this.btnGoodBye.Location = new System.Drawing.Point(39, 92);
+            this.btnGoodBye.Name = "btnGoodBye";
+            this.btnGoodBye.Size = new System.Drawing.Size(128, 23);
+            this.btnGoodBye.TabIndex = 2;
+            this.btnGoodBye.Text = "Say Good bye!!!";
+            this.btnGoodBye.UseVisualStyleBackColor = true;
+            this.btnGoodBye.Click += new System.EventHandler(this.btnGoodBye_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGoodBye);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmMain";
+            this.Text = "Hello App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.Button btnGoodBye;
+    }
+}
+
---
+++ project-1/frmMain.cs
@@ -0,0 +1,37 @@
+﻿//frmMain.cs
+// CS-1181
+// Sagar Gaire
+// 9/5/2017
+// Anil Mandal
+// From this project we can learn to change and display any name on Text box
+// WOW: I have created a new bottom Good Bye. If we type somethings in text box than it will show somethings good bye in a message box.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+         MessageBox.Show("Hello " + txtName.Text);
+        }
+
+        private void btnGoodBye_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Good Bye " + txtName.Text);
+        }
+    }
+}
